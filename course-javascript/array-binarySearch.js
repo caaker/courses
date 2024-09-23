6d5c4b1d00d00ef2f08cd964cc0f77f5ea4e35da
Aaker <caaker@austin.eid.utexas.edu>
2024-09-23 15:30:15 -0500

    added comments to binarySearch

--- course-javascript/array-binarySearch.js
+++ course-javascript/array-binarySearch.js
@@ -4,14 +4,23 @@ function binarySearch(arr, target) {
   let start = 0;
   let end = arr.length;
   while(start < end) {
-    let mid = Math.round((start + end - 1) / 2);
-    console.log(start, mid, end)
+    
+	// calculate the middle index; 
+	let mid = Math.round((start + end - 1) / 2);
+    
+	// console.log(start, mid, end)
+	
+	// target has been found; return the index
     if(target === arr[mid]) {
       return mid;
     }
+	
+	// target is less than the middle value; make the end index the new middle index
     if(target < arr[mid]) {
       end = mid;
     }
+	
+	// target is greater than the middle value;  make the start index the new middle index
     if(target > arr[mid]) {
       start = mid;
     }
@@ -25,7 +34,7 @@ console.log(binarySearch(arr, 7));
 
   Notes:
 
-    Write again using recursion
+    Write again using recursion.  Consider overflow for mid calculation.
 
   Algorithm:
 
