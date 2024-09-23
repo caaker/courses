7faad9eaf2225fe09e7bfaf20f2d2662374df364
caaker <caaker.0@gmail.com>
2024-09-23 13:20:29 -0500

    Update 01-types.js
    added primitive types section

--- course-javascript/01-types.js
+++ course-javascript/01-types.js
@@ -1,11 +1,18 @@
-// 7 main "types" - number, string, array, hash, function, null, undefined
+// JavaScript has 7 primitive types
+// https://developer.mozilla.org/en-US/docs/Glossary/Primitive
 const number = 1;
 const string = 'string';
+// boolean
+// symbol
+// bigint
+const und = undefined;
+const nul = null;
+
+//
 const array = ['val1', 'val2'];
 const hash = {prop1: 'val1'};
 const fun = function() {};
-const nul = null;
-const und = undefined;
+
 
 // Note that only null and undefined will throw an error if you try to access a non-existent property on them
 number.prop;
@@ -15,7 +22,7 @@ hash.prop;
 fun.prop;
 
 
-// Note that typeof is broken for array and null
+// Note that typeof is broken for null
 console.log('TYPEOF\n');
 console.log(typeof number);
 console.log(typeof string);
@@ -25,7 +32,8 @@ console.log(typeof fun);
 console.log(typeof nul);    // returns object
 console.log(typeof und);
 
-// we can replace the operator typeof with the function typeOf to fix the null and array issue
+// we can replace the operator typeof with the function typeOf to fix the null issue
+// furthermore, it can now differentiate between an object literal and and array literal
 function typeOf(test) {
   let ret;
   if(Array.isArray(test)) {
