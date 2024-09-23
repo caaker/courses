b0eaf69cf31da9415257e3cf70f76c63d0ed5572
Aaker <caaker@austin.eid.utexas.edu>
2024-09-23 14:08:50 -0500

    added comments

--- course-javascript/graph.mjs
+++ course-javascript/graph.mjs
@@ -1,5 +1,7 @@
+// a graph implemented as an adjacency list which is implemented as a hash map with arrays
 class Graph {
 
+  // top structure is a map
   constructor() {
     this.adjacency_list = new Map();
   }
@@ -8,6 +10,7 @@ class Graph {
     this.adjacency_list.set(vertex, []);
   }
 
+  // secondary structure is an array
   addEdge(src, dst) {
     this.adjacency_list.get(src).push(dst);
     this.adjacency_list.get(dst).push(src)
@@ -15,10 +18,14 @@ class Graph {
 
   printGraph() {
     const keys = this.adjacency_list.keys();
+  
+	// loop through keys of the hash map
     for (let i of keys) {
       const values = this.adjacency_list.get(i);
       let string = '';
-      for (let j of values) {
+      
+	  // loop through each array in the hash map
+	  for (let j of values) {
         string += j + ' ';
       }
       console.log(i + ' -> ' + string);
@@ -26,10 +33,14 @@ class Graph {
   }
 
   makeGraph() {
+	  
+	// add vertices
     const vertices = [ 'A', 'B', 'C', 'D', 'E', 'F' ];
     for (let i = 0; i < vertices.length; i++) {
       this.addVertex(vertices[i]);
     }
+	
+	// add edges
     this.addEdge('A', 'B');
     this.addEdge('A', 'D');
     this.addEdge('A', 'E');
