02facb1581077484b5975475db785773ac788d38
caaker <caaker.0@gmail.com>
2024-10-15 09:30:29 -0500

    Update 03-arrays-sort.c

--- course-c/03-arrays-sort.c
+++ course-c/03-arrays-sort.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// this function must match the signature required by qsort below
+// (int*) casts the generic pointer to an int pointer
+//  * dereferences the pointer to an integer value
 int compare(const void* a, const void* b) {
     return (*(int*)a - *(int*)b);
 }
 
 int main() {
   int numbers[] = {1, 2, 3, 2, 1};
+
+  // this is a common c idiom for calculating the length of an array
   int length = sizeof(numbers) / sizeof(numbers[0]);
   qsort(numbers, length, sizeof(int), compare);
 
